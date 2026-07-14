bcfbbfbe8d5c0cab97dc4b8d914017772f5d0af8
vetraxdeano-wq <vetraxdeano@gmail.com>
2026-07-14 13:17:43 +0200

    Delete run42/js directory

--- run42/js/supabase-client.js
+++
@@ -1,8 +0,0 @@
-import { CONFIG } from "./config.js";
-
-// Charge le SDK Supabase depuis le CDN (voir index.html)
-// et expose un client prêt à l'emploi.
-export const supabase = window.supabase.createClient(
-  CONFIG.SUPABASE_URL,
-  CONFIG.SUPABASE_ANON_KEY
-);
